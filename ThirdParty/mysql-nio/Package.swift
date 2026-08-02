1193af2daf5d450abebb44bcc9d23b31ad52cfc0
Wagner Fernandes <wagner@localhost>
2026-08-02 14:52:59 -0300

    fix: vendor MySQLNIO como arquivos reais (sem gitlink)

---
+++ ThirdParty/mysql-nio/Package.swift
@@ -0,0 +1,52 @@
+// swift-tools-version:6.0
+import PackageDescription
+
+let package = Package(
+    name: "mysql-nio",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
+    ],
+    products: [
+        .library(name: "MySQLNIO", targets: ["MySQLNIO"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "5.0.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.14.0"),
+    ],
+    targets: [
+        .target(
+            name: "MySQLNIO",
+            dependencies: [
+                .product(name: "Algorithms", package: "swift-algorithms"),
+                .product(name: "_CryptoExtras", package: "swift-crypto"),
+                .product(name: "Crypto", package: "swift-crypto"),
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "NIOSSL", package: "swift-nio-ssl"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "MySQLNIOTests",
+            dependencies: [
+                .target(name: "MySQLNIO"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+    ]
+)
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("ExistentialAny"),
+    // .enableUpcomingFeature("InternalImportsByDefault"),
+    .enableUpcomingFeature("MemberImportVisibility"),
+    .enableUpcomingFeature("InferIsolatedConformances"),
+    // .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
+    .enableUpcomingFeature("ImmutableWeakCaptures"),
+] }
---
+++ ThirdParty/mysql-nio/Sources/MySQLNIO/Exports.swift
@@ -0,0 +1,6 @@
+@_documentation(visibility: internal) @_exported import NIO
+@_documentation(visibility: internal) @_exported import struct Logging.Logger
+@_documentation(visibility: internal) @_exported import struct NIOSSL.TLSConfiguration
+@_documentation(visibility: internal) @_exported import struct Foundation.Date
+@_documentation(visibility: internal) @_exported import struct Foundation.UUID
+@_documentation(visibility: internal) @_exported import struct Foundation.Decimal
